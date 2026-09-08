b334c379fc6ffcb0276e19c0c1dd2401f4d0d7af
tanyabareja16 <tanyabareja12900@gmail.com>
2026-09-08 05:55:03 -1200

    Add time conversion program in hours_min_sec.c
    Implement a program to convert seconds to hours, minutes, and seconds format.

---
+++ day5/hours_min_sec.c
@@ -0,0 +1,33 @@
+//Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+/*
+Sample Test Cases:
+Input 1:
+3661
+Output 1:
+1:1:1
+
+Input 2:
+7322
+Output 2:
+2:2:2
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int totalSeconds, hours, minutes, seconds;
+
+    scanf("%d", &totalSeconds);
+
+    hours = totalSeconds / 3600;
+    totalSeconds = totalSeconds % 3600;
+
+    minutes = totalSeconds / 60;
+    seconds = totalSeconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, seconds);
+
+    return 0;
+}
