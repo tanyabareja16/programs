90cd9400daf0cd2a3cdcded412720e3f1a58fc17
tanyabareja16 <tanyabareja12900@gmail.com>
2026-09-08 05:16:22 -1200

    Implement sum, difference, product, and quotient

---
+++ day 1 '/sum_of_difference_of_product.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d",
+           a + b, a - b, a * b, a / b);
+
+    return 0;
+}
