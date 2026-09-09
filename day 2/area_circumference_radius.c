e75c415f1c6b821b379427b5fb9518a72a146f0d
tanyabareja16 <tanyabareja12900@gmail.com>
2026-09-08 16:38:28 -1200

    Create area_circumference_radius.c

---
+++ day 2/area_circumference_radius.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+    const float PI = 3.14159;
+
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    printf("Area=%.2f, Circumference=%.2f", area, circumference);
+
+    return 0;
+}
