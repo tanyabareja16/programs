67dcd3aee2715a5e7e71940a21060239e87a19ad
tanyabareja16 <tanyabareja12900@gmail.com>
2026-09-10 06:13:13 -1200

    Add quadratic equation root finder
    Implement a program to find roots of a quadratic equation based on user input.

---
+++ day9/root_of_quadratic_equation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+    float discriminant, root1, root2, realPart, imaginaryPart;
+
+    scanf("%f %f %f", &a, &b, &c);
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+
+        printf("Roots are real and different: %.0f, %.0f", root1, root2);
+    }
+    else if (discriminant == 0) {
+        root1 = -b / (2 * a);
+
+        printf("Roots are real and same: %.0f", root1);
+    }
+    else {
+        realPart = -b / (2 * a);
+        imaginaryPart = sqrt(-discriminant) / (2 * a);
+
+        printf("Roots are complex");
+    }
+
+    return 0;
+}
